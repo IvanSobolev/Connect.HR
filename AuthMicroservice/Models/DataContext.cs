8adbc23ff14a272f1b69e5799bbe9b5b6711d379
IvanSobolev <i@bezlikiy.ru>
2025-04-16 02:17:18 +0300

    feat: db context

---
+++ AuthMicroservice/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthMicroservice.Models;
+
+public class DataContext : DbContext
+{
+    public DbSet<Profile> Profiles { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Profile>(m =>
+        {
+            m.HasKey(p => p.Id);
+            m.HasMany(p => p.RefreshTokens)
+                .WithOne(r => r.Profile);
+        });
+    }
+}
