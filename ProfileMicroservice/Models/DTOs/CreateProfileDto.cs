5788143207e94bb80ccbbad243198309dcc0ebb9
IvanSobolev <i@bezlikiy.ru>
2025-04-16 01:10:08 +0300

    feat: add Dtos

---
+++ ProfileMicroservice/Models/DTOs/CreateProfileDto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TinderAPI.Models.DTOs;
+
+public class CreateProfileDto
+{
+    [Required]
+    public string FirstName { get; set; }
+        
+    [Required]
+    public string LastName { get; set; }
+        
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+        
+    [Required]
+    public string Password { get; set; }
+        
+    public bool IsMale { get; set; }
+    public DateOnly BirthdayDate { get; set; }
+    public string? Description { get; set; }
+}
---
+++ ProfileMicroservice/Models/DTOs/PhotoDto.cs
@@ -0,0 +1,8 @@
+﻿namespace TinderAPI.Models.DTOs;
+
+public class PhotoDto
+{
+    public long PhotoId { get; set; }
+    public string PhotoUrl { get; set; }
+    public bool IsMain { get; set; }
+}
