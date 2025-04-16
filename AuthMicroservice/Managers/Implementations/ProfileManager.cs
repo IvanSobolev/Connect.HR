6dcf14014ef9bf624e3b55e9bfb8ad96446b97fa
IvanSobolev <i@bezlikiy.ru>
2025-04-16 04:35:09 +0300

    fix: grps

---
+++ AuthMicroservice/Managers/Implementations/ProfileManager.cs
@@ -0,0 +1,24 @@
+﻿using AuthMicroservice.Managers.Interfaces;
+using AuthMicroservice.Models.Dtos;
+using AuthMicroservice.Repositories.Interfaces;
+using Grpc.Net.Client;
+using TinderAPI;
+
+namespace AuthMicroservice.Managers.Implementations;
+
+public class ProfileManager ( IProfileRepository profileRepository ) : IProfileManager
+{
+    private readonly IProfileRepository _profileRepository = profileRepository;
+    public async Task<(string RefreshToken, string AcsesToken)> RegistrationAsync(RegistrationDto profile)
+    {
+        if (await _profileRepository.ExistsByEmailAsync(profile.Email))
+        {
+            return new ValueTuple<string, string>("", "");
+        }
+        
+        using var channel = GrpcChannel.ForAddress("http://localhost:5001");
+        var client = new CreateProfileService.CreateProfileServiceClient(channel);
+
+        //var reply = await client.CreateProfile(new CreateProfileRequest { });
+    }
+}
---
+++ AuthMicroservice/Managers/Interfaces/IProfileManager.cs
@@ -0,0 +1,9 @@
+﻿using AuthMicroservice.Models;
+using AuthMicroservice.Models.Dtos;
+
+namespace AuthMicroservice.Managers.Interfaces;
+
+public interface IProfileManager
+{
+    Task<(string RefreshToken, string AcsesToken)> RegistrationAsync(RegistrationDto profile);
+}
