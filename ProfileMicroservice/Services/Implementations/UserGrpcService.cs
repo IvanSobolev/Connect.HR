6e876d40773f84c33ac081d02a9cacc0fcd73df5
IvanSobolev <i@bezlikiy.ru>
2025-04-21 22:16:22 +0300

    feat: profile gRPC integration

---
+++ ProfileMicroservice/Services/Implementations/UserGrpcService.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Core;
+
+namespace TinderAPI.Services.Implementations;
+
+public class UserGrpcService : CreateProfileService.CreateProfileServiceBase
+{
+    public async override Task<CreateProfileResponse> CreateProfile(CreateProfileRequest request, ServerCallContext context)
+    {
+        Console.WriteLine(request.FirstName);
+        return new CreateProfileResponse { Success = true, ProfileId = request.Id, ErrorMessage = String.Empty };
+    }
+}
