5c0b18abece054f01c3d36e271c1b43fc1cf11a6
IvanSobolev <i@bezlikiy.ru>
2025-04-23 12:12:41 +0300

    feat: Grpc endpoint

--- ProfileMicroservice/Services/Implementations/ProfileGrpcService.cs
+++ ProfileMicroservice/Services/Implementations/ProfileGrpcService.cs
@@ -1,12 +1,26 @@
 ﻿using Grpc.Core;
+using TinderAPI.Models.Entitys;
+using TinderAPI.Repositories.Interfaces;
 
 namespace TinderAPI.Services.Implementations;
 
-public class ProfileGrpcService : CreateProfileService.CreateProfileServiceBase
+public class ProfileGrpcService (IProfileRepository profileRepository) : CreateProfileService.CreateProfileServiceBase
 {
-    public async override Task<CreateProfileResponse> CreateProfile(CreateProfileRequest request, ServerCallContext context)
+    private readonly IProfileRepository _profileRepository = profileRepository;
+    public override async Task<CreateProfileResponse> CreateProfile(CreateProfileRequest request, ServerCallContext context)
     {
-        Console.WriteLine(request.FirstName);
+        await _profileRepository.AddAsync(new Profile
+        {
+            Id = Guid.Parse(request.Id),
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            CreatedAt = DateTime.UtcNow,
+            LastActive = DateTime.UtcNow,
+            IsMale = request.IsMale,
+            BirthdayDate = DateOnly.Parse(request.Birthday),
+            Latitude = request.Latitude,
+            Longitude = request.Longitude
+        });
         return new CreateProfileResponse { Success = true, ProfileId = request.Id, ErrorMessage = String.Empty };
     }
 }
