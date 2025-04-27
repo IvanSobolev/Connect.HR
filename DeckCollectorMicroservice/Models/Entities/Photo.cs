db8672e91e713e629f885e9d3f398f0493fba2aa
IvanSobolev <i@bezlikiy.ru>
2025-04-27 10:08:39 +0300

    feat: models and db context for connection

---
+++ DeckCollectorMicroservice/Models/Entities/Photo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeckCollectorMicroservice.Models.Entities;
+
+public class Photo
+{
+    public long PhotoId { get; set; }
+    public Guid ProfileId { get; set; }
+    [Url]
+    public string PhotoUrl { get; set; }
+    public bool IsMain { get; set; }
+    
+    public Profile Profile { get; set; }
+}
