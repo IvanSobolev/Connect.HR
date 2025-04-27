20e9db46e03374b9458d7d926f52195cc9260c7b
IvanSobolev <i@bezlikiy.ru>
2025-04-27 10:26:21 +0300

    feat: dtos and repository interface

---
+++ DeckCollectorMicroservice/Models/DTOs/PhotoDto.cs
@@ -0,0 +1,9 @@
+﻿namespace DeckCollectorMicroservice.Models.DTOs;
+
+public class PhotoDto
+{
+    public long PhotoId { get; set; }
+    public Guid ProfileId { get; set; }
+    public string PhotoUrl { get; set; }
+    public bool IsMain { get; set; }
+}
---
+++ DeckCollectorMicroservice/Models/DTOs/ProfileDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DeckCollectorMicroservice.Models.DTOs;
+
+public class ProfileDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime LastActive { get; set; }
+    public bool IsMale { get; set; }
+    public int Age { get; set; }
+    public string? Description { get; set; }
+    public double DistanceKm { get; set; }
+    public ICollection<HobbyDto> Hobbies { get; set; }
+    public ICollection<PhotoDto> Photos { get; set; }
+}
